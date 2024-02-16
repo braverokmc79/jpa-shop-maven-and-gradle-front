da9c60c51972e7c24b39f617e1f124f5f26d91c1
braverokmc79 <braverokmc79@gmail.com>
2024-02-16 13:55:42 +0900

    build 디렉토리 추가

---
+++ build/static/js/444.b6dc458c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfront_end=self.webpackChunkfront_end||[]).push([[444],{8444:(e,n,s)=>{s.r(n),s.d(n,{default:()=>j});var r=s(2672),c=s(9060),t=s(9304),d=s(2496);const a=()=>(0,d.jsxs)(r.Wq,{children:[(0,d.jsx)(r.KU,{name:"App",children:(0,d.jsx)(r.Yl,{name:"Loader",children:(0,d.jsx)(r.Y$,{children:(0,d.jsx)(l,{})})})}),(0,d.jsx)(t.c,{})]});function l(){return(0,d.jsx)(c.Fragment,{children:"Loading..."})}const j=()=>(0,d.jsx)(r.oF,{palette:(0,d.jsx)(a,{})})}}]);
+//# sourceMappingURL=444.b6dc458c.chunk.js.map
