4c4d3718f442bc0d6961228fcf157fcf4cb0b707
braverokmc79 <braverokmc79@gmail.com>
2024-03-15 15:37:16 +0900

    update

--- src/api-config.js
+++ src/api-config.js
@@ -1,13 +1,8 @@
 let backendHost;
-
 const hostname =window && window.location && window.location.hostname;
-
 
 if(hostname==="localhost"){
    backendHost="http://localhost:5000";
-   //backendHost="http://mcaronics-todo-api-service.ap-northeast-2.elasticbeanstalk.com";
-   //backendHost="https://ma7server.p-e.kr";
-   //backendHost="http://macaronics.iptime.org:5000"
 }else{
   backendHost="https://ma7server.p-e.kr";
 }
